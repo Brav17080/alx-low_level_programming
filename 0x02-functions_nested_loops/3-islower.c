b5b2df8248746e364c46994c19385d15e4e7df79
brav17080 <bravin17080@gmail.com>
2022-12-14 16:41:10 -0800

    task 0 and 1

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * checks for lowercase letters
+ * @c: parameter
+ * Return: 0 (success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('n');
+}
