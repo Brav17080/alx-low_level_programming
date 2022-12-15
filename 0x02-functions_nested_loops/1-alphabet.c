8dee328817d6dea35b825c23a3d3e6bb94bcca92
brav17080 <bravin17080@gmail.com>
2022-12-14 16:05:05 -0800

    lets do this better

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - program prints alphabets a-z
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This ia a README for c functions, nested loops
