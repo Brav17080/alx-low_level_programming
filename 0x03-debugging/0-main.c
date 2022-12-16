e911efe76b609d094c9c12f029e89f2db48ad0e9
brav17080 <bravin17080@gmail.com>
2022-12-15 16:53:55 -0800

    Test positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -10,19 +8,11 @@
 
 
 int main(void)
-
 {
-
 	 int i;
 
+	 i = 0;
+	 positive_or_negative(i);
 
-
-	  i = 0;
-
-	   positive_or_negative(i);
-
-
-
-	   return (0);
-
+	 return (0);
 }
